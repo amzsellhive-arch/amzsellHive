a2d3b4ad18468e0e7d68d17b6fe93572686778a2
amilokz <amilokz1@gmail.com>
2026-08-21 16:41:28 +0500

    fix: pages ui import paths

--- frontend/src/pages/LogoutCallbackPage.jsx
+++ frontend/src/pages/LogoutCallbackPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import LoadingSpinner from '../components/ui/LoadingSpinner';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
 
 const LogoutCallbackPage = () => {
   useEffect(() => {
